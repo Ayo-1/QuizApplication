af6cd3a24827b23d80fdd8547085f93bc8bc8cd8
Ayo-1 <52506241+Ayo-1@users.noreply.github.com>
2020-04-09 08:13:47 +0600

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -3,15 +3,7 @@ require('dotenv').config();
 const db = {}
 const seq = new sequelize("uGATUFLj27", "uGATUFLj27", "WyB1OUnNro", {
 	host: "remotemysql.com",
-	dialect: "mysql",
-	operatorAliases: false,
-
-	pool: {
-		max: 5,
-		min: 0,
-		acquire: 30000,
-		idle: 10000
-	}
+	dialect: "mysql"
 })
 db.seq = seq
 module.exports = db;

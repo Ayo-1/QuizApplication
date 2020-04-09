08dee0aa6a0cfd0bc18d0981d051f81ae2f397ad
Ayo-1 <52506241+Ayo-1@users.noreply.github.com>
2020-04-09 07:35:59 +0600

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,8 @@
 const sequelize = require('sequelize')
 require('dotenv').config();
 const db = {}
-const seq = new sequelize("quiz_db", "root", "", {
-	host: "localhost",
+const seq = new sequelize("uGATUFLj27", "uGATUFLj27", "WyB1OUnNro", {
+	host: "remotemysql.com",
 	dialect: "mysql",
 	operatorAliases: false,
 
